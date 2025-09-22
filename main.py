7e21ad48c0b4930b89f821a94dc1478fc91d15dc
Pushpender Sharma <pushpendersharma2019@gmail.com>
2025-09-22 14:49:01 +0530

    Refactor Ollama LLM initialization and update question for clarity

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 from langchain_community.utilities.sql_database import SQLDatabase
 from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
 from langchain_community.agent_toolkits.sql.base import create_sql_agent
-from langchain_ollama import OllamaLLM
+from langchain_ollama import ChatOllama
 from langchain_google_genai import ChatGoogleGenerativeAI
-
+from langchain.agents.agent_types import AgentType
 
 # Load environment variables
 load_dotenv()
@@ -24,18 +24,17 @@ print(db.get_usable_table_names())
 
 # 2. Initialize the Ollama LLM
 
-llm = OllamaLLM(
-    base_url=ollama_url,
-    model=model_name,
-    temperature=0,
-    handle_parsing_errors=True,
-)
+# llm = ChatOllama(
+#     base_url=ollama_url,
+#     model=model_name,
+#     temperature=0,
+# )
 
 # Initialize Google Gemini model (Gemini 2.5 or others)
-# llm = ChatGoogleGenerativeAI(
-#     model=gemini_model_name,
-#     api_key=google_api_key,
-# )
+llm = ChatGoogleGenerativeAI(
+    model=gemini_model_name,
+    api_key=google_api_key,
+)
 
 # 3. Create the SQL toolkit (this includes all the SQL tools like query, schema, etc.)
 toolkit = SQLDatabaseToolkit(db=db, llm=llm)
@@ -44,13 +43,13 @@ toolkit = SQLDatabaseToolkit(db=db, llm=llm)
 agent_executor = create_sql_agent(
     llm=llm,
     toolkit=toolkit,
+    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
     verbose=True,
     handle_parsing_errors=True,
 )
 
 # 5. Ask a question
-# question = "List down the different PIIs"
-question = "List down the past data breaches with their details."
+question = "What are the different risk registered into the system for customer_id 129"
 
 # 6. Run the agent
 try:
